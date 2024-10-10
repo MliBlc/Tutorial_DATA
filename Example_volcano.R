af03d7e60583ad834c2360ddfa23d4368009f759
Mehmet Ali <132394187+MliBlc@users.noreply.github.com>
2024-10-10 22:35:34 +0300

    Update Example_volcano.R

--- Example_volcano.R
+++ Example_volcano.R
@@ -1,5 +1,5 @@
 ggplot(as.data.frame(res1), aes(x = log2FoldChange, y = -log10(padj))) +
-  geom_point(aes(color = abs(log2FoldChange) > 1d & padj < 0.05), alpha = 0.5) +
+  geom_point(aes(color = abs(log2FoldChange) > 1 & padj < 0.05), alpha = 0.5) +
   scale_color_manual(values = c("black", "red"), guide = FALSE) +  # Red points for significant changes
   geom_hline(yintercept = -log10(padj_threshold), linetype = "dashed", color = "blue") +  # Horizontal threshold line
   geom_vline(xintercept = c(-log2fc_threshold, log2fc_threshold), linetype = "dashed", color = "blue") +  # Vertical threshold lines
